b88b974e69eb9db1cbe0b3afd984fe6b3b6786dc
Khuong Tran <dbcgdhh@gmail.com>
2025-03-24 16:52:47 +0700

    Init

---
+++ nuxt.config.ts
@@ -0,0 +1,52 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: "2024-11-01",
+  devtools: { enabled: true },
+  modules: [
+    "nuxt-gtag",
+    "@nuxt/image",
+    "@nuxt/ui",
+    "@nuxt/eslint",
+    "@nuxt/content",
+  ],
+  image: {
+    quality: 70,
+    format: ["webp"],
+    screens: {
+      xs: 512,
+      sm: 640,
+      md: 768,
+      lg: 1024,
+      xl: 1280,
+      xxl: 1536,
+      "2xl": 1536,
+    },
+    presets: {
+      before_after: {
+        modifiers: {
+          width: 512,
+        },
+      },
+    },
+    densities: [1, 2],
+    provider: "cloudflare",
+    cloudflare: { baseURL: "https://cdn.betterpic.io" },
+  },
+  vite: {
+    build: {
+      target: "esnext",
+    },
+  },
+  nitro: {
+    preset: "vercel",
+  },
+  content: {
+    markdown: {
+      rehypePlugins: {
+        "rehype-external-links": false,
+      },
+    },
+    locales: ["en", "es"],
+    defaultLocale: "en",
+  },
+});
